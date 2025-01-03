6941334ad90365eea8cc48f062deb94fefddec9a
Xotam <fobus1289@gmail.com>
2025-01-03 07:30:24 +0500

    feat: add case conversion functions

--- change_case.go
+++ change_case.go
@@ -1,26 +1,107 @@
 package parser
 
-import "strings"
+import (
+	"reflect"
+	"strings"
+	"unicode"
+	"unsafe"
+)
+
+func CamelCaseOptimized(input string) string {
+	var result strings.Builder
+	result.Grow(len(input))
+
+	capitalizeNext := false
+	for i, r := range input {
+		if unicode.IsSpace(r) || unicode.IsPunct(r) || unicode.IsSymbol(r) {
+			capitalizeNext = true
+			continue
+		}
+
+		if capitalizeNext || i == 0 {
+			result.WriteRune(unicode.ToUpper(r))
+			capitalizeNext = false
+		} else {
+			result.WriteRune(unicode.ToLower(r))
+		}
+	}
+
+	res := []rune(result.String())
+	res[0] = unicode.ToLower(res[0])
+	return string(res)
+}
+
+func CamelCaseUnsafe(input string) string {
+	// Преобразуем строку в байтовый срез без копирования
+	inBytes := *(*[]byte)(unsafe.Pointer(&input))
+	// outBytes := make([]byte, 0, len(inBytes)) // Создаём выходной массив с избыточной длиной
+
+	outSliceHeader := &reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(&inBytes[0])),
+		Len:  0,
+		Cap:  len(inBytes),
+	}
+
+	outBytes := *(*[]byte)(unsafe.Pointer(outSliceHeader))
+
+	capitalizeNext := true
+	for _, b := range inBytes {
+		if b == '_' || b == '-' || b == ' ' || unicode.IsPunct(rune(b)) { // Проверяем разделители
+			capitalizeNext = true
+			continue
+		}
+		if capitalizeNext {
+			// Преобразуем к верхнему регистру
+			if b >= 'a' && b <= 'z' {
+				b -= 'a' - 'A'
+			}
+			capitalizeNext = false
+		} else {
+			// Преобразуем к нижнему регистру
+			if b >= 'A' && b <= 'Z' {
+				b += 'a' - 'A'
+			}
+		}
+		outBytes = append(outBytes, b)
+	}
+
+	// Преобразуем байты обратно в строку
+	return *(*string)(unsafe.Pointer(&outBytes))
+}
 
 func CamelCase(s string) string {
 	p := NewParser(s)
 
-	var result []byte
+	var result = make([]byte, 0, len(s))
 
-	tokens := Filter(p.Parse(), func(t Token) bool {
-		return t.Type == IDENT
-	})
+	// tokens := Filter(p.Parse(), func(t Token) bool {
+	// 	return t.Type == IDENT
+	// })
 
-	for i, token := range tokens {
+	for i, token := range p.Parse() {
 		if token.Type == IDENT {
 			res := token.Join(token.SplitUpper, func(s string) string {
 				if len(s) == 0 {
 					return s
 				}
+
 				if i == 0 {
-					return strings.ToLower(s[0:1]) + s[1:]
+					q := []rune(s)
+					if unicode.IsLower(q[0]) {
+						return s
+					}
+					q[0] = unicode.ToLower(q[0])
+					return string(q)
 				}
-				return strings.ToUpper(s[0:1]) + s[1:]
+
+				q := []rune(s)
+
+				if unicode.IsUpper(q[0]) {
+					return s
+				}
+
+				q[0] = unicode.ToUpper(q[0])
+				return string(q)
 			})
 
 			result = append(result, res...)
--- paser_test.go
+++ paser_test.go
@@ -7,16 +7,74 @@ import (
 
 func TestFindTexts(t *testing.T) {
 
-	const EXP = " Testing_@@@testing _______testing"
-
-	fmt.Println(SnakeCase(EXP))
-	fmt.Println(CamelCase(EXP))
+	const EXP = `
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+	`
+	// fmt.Println(SnakeCase(EXP))
+	// fmt.Println(CamelCase(EXP))
+	// fmt.Println(CamelCaseOptimized(EXP))
+	fmt.Println(CamelCaseUnsafe(EXP))
 }
 
 func BenchmarkSnakeCase(b *testing.B) {
-	const EXP = " Testing_@@@testing _______testing"
+	const EXP = " Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing"
 	for i := 0; i < b.N; i++ {
 		_ = SnakeCase(EXP)
+	}
+}
+
+func BenchmarkCamelCase(b *testing.B) {
+	const EXP = `
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+	`
+	for i := 0; i < b.N; i++ {
+		_ = CamelCase(EXP)
+	}
+}
+
+func BenchmarkSnakeCase_2(b *testing.B) {
+
+	const EXP = " id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing"
+	parser := NewParser(EXP)
+
+	for i := 0; i < b.N; i++ {
+		_ = parser.ParsePlaceholders()
+	}
+}
+
+// BenchmarkCamelCaseOptimized-32    	  135783	      8839 ns/op	    5760 B/op	       3 allocs/op
+func BenchmarkCamelCaseOptimized(b *testing.B) {
+	const EXP = `
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+	`
+	for i := 0; i < b.N; i++ {
+		_ = CamelCaseOptimized(EXP)
+	}
+}
+
+// BenchmarkCamelCaseUnsafe-32    	 3132452	       386.3 ns/op	     288 B/op	       1 allocs/op
+func BenchmarkCamelCaseUnsafe(b *testing.B) {
+	const EXP = `
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+		id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY} id:{ID}/name:{NAME}/age:${AGE}/salary:{SALARY}Testing_@@@testing _______testing Testing_@@@testing _______testing Testing_@@@testing _______testingTesting_@@@testing _______testingTesting_@@@testing _______testing
+	`
+	for i := 0; i < b.N; i++ {
+		_ = CamelCaseUnsafe(EXP)
 	}
 }
 
